587d28b9b08c824efe62685e21483728951c567c
Tomoyuki KAMIYA <kamiya7140@gmail.com>
2017-11-26 23:29:58 +0900

    add user_profile model

---
+++ db/migrate/20171126091425_create_user_profiles.rb
@@ -0,0 +1,10 @@
+class CreateUserProfiles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_profiles do |t|
+      t.string :showname
+      t.references :user
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126074046) do
+ActiveRecord::Schema.define(version: 20171126091425) do
+
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "showname"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
